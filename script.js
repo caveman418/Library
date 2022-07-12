60e05414868491799aaa8a09f77f087ee65c52f9
caveman418 <104240436+caveman418@users.noreply.github.com>
2022-07-11 20:37:38 -0500

    final css
    basic css addition

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-let myLibrary = [];
+let myLibrary = [{title: 'The Hunger Games', author: 'Suzanna Collins', pages: 374, status: true},{title: 'Catching Fire', author: 'Suzanna Collins', pages: 391, status: false}];
 
 class Book {
-    constructor(title, author, pages, read) {
+    constructor(title, author, pages, status) {
         this.title = title;
         this.author = author;
         this.pages = pages;
-        this.read = read;
+        this.status = status;
     }
     info() {
-        let haveRead = (this.read) ? 'already finished':'not read yet';
+        let haveRead = (this.status) ? 'already finished':'not read yet';
         return this.title + ' by ' + this.author + ', ' + this.pages + ' pages, ' + haveRead;
     }
 }
 
 function addBookToLibrary() {
-    
+
 }
 
-
-// const hungerGames = new Book('The Hunger Games', 'Suzanna Collins', 374, true);
+function createTable(myLibrary) {
+    for (let i=0; i<Object.keys(myLibrary).length; i++) {
+        const tr = document.createElement('tr');
+        for (let key in myLibrary) {
+            
+        }
+    }
+}
